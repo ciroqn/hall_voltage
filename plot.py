886bb2c2f87cd344c5134faabb27100e78af5eef
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2023-01-01 16:39:00 +0000

    Update plot.py

--- plot.py
+++ plot.py
@@ -7,3 +7,14 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("calibration_hall.csv", sep=",", header=2)
 
 df 
+
+# get data for average magnetic field strength (in mT) and coil current (in A) from calibration
+with open("calibration_hall.csv", mode="r") as calib_points:
+    avg_mag = df["Average"]
+    coil_current = df["I_c / A"]
+    
+# convert to arrays
+avg_mag_array = avg_mag.array
+coil_current_array = coil_current.array
+
+
