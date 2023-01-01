dc3fbb285aa09d20e139a3d9dc3e333687d0e98a
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2023-01-01 16:40:05 +0000

    Update plot.py

--- plot.py
+++ plot.py
@@ -17,4 +17,22 @@ with open("calibration_hall.csv", mode="r") as calib_points:
 avg_mag_array = avg_mag.array
 coil_current_array = coil_current.array
 
+############# Plotting calibration data (i.e. mag. field strength vs varying coil currents ##############
+# Sete plot parameters
+plt.rcParams["figure.figsize"] = [12, 8]
+plt.rcParams["axes.titlesize"] = 20
+plt.rcParams["axes.labelsize"] = 16
+plt.rcParams["xtick.labelsize"] = 16
+plt.rcParams["ytick.labelsize"] = 16
+plt.rcParams["legend.fontsize"] = 16
 
+# setup
+plt.axhline(y=0, color="black", linewidth=1)
+plt.axvline(x=0, color="black", linewidth=1)
+plt.title("Magnetic field strength as a function of coil current")
+plt.xlabel("Coil current / A")
+plt.ylabel("Magnetic field strength / mT")
+
+# Plot
+plt.scatter(coil_current_array, avg_mag_array)
+plt.show()
