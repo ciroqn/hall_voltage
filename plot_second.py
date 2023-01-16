efbdbccc656c290029e746a3795508c09819efeb
Marcus Watson <79422987+ciroqn@users.noreply.github.com>
2023-01-16 22:20:28 +0000

    hall voltage plot to find hall coefficient

--- plot_second.py
+++ plot_second.py
@@ -31,4 +31,42 @@ plt.ylabel("Magnetic field strength / mT")
 plt.scatter(df['Coil Current / A'], df['B Avg / mT'])
 plt.show()
 
+######### hall voltage ###########
+
+# Hall voltage in volts
+vh_yaxis = df["V_H / muV"]*10**-6
+
+# magnetic field strength in Teslas
+bstrength_T = df["B / mT"]*10**-3
+
+# we want to find gradient of graph, so we find the variable, x, which consists of a range of terms
+def x_axis(B):
+    # transverse current at a constant 10 A
+    I_t = 10
+    # thickness of sample 
+    t = 50*10**-6
+    return (I_t*B)/t
+
+find_x_axis = x_axis(bstrength_T)
+
+best_fit = linregress(find_x_axis, vh_yaxis)
+
+print("************************************")
+print(f"Gradient: {best_fit.slope} +/- {best_fit.stderr:.4f}")
+print(f"Intercept: {best_fit.intercept} +/- {best_fit.intercept:.4f}")
+print(f"R-squared value: {best_fit.rvalue**2}")
+print("************************************")
+
+ylin = best_fit.slope*find_x_axis + best_fit.intercept
+
+plt.axhline(y=0, color="black", linewidth=1)
+plt.axvline(x=0, color="black", linewidth=1)
+plt.title(r"$ V_{H}$  against  $\frac{I_{T} \times B}{t}$")
+plt.xlabel(r"$\frac{I_{T} \times B}{t}$ / $\frac{AT}{m}$")
+plt.ylabel("Hall voltage / V")
+
+plt.scatter(find_x_axis.array, vh_yaxis.array, label="Data points")
+plt.plot(find_x_axis, ylin, color="red", label="Best fit")
+plt.legend()
+plt.show()
 
